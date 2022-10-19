cb7b84e4e350dc6fb4fa9bcd5cba07b6bde3ee66
Kunal <mailto.guptakunal@gmail.com>
2022-10-20 01:56:24 +0530

    assinment 1 of crud with association

---
+++ block-BNaadl/blog/models/article.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    tags: String,
+    author: String,
+    likes: Number,
+  },
+  { timestamps: true }
+);
+
+var Article = mongoose.model('Article', articleSchema);
+module.exports = Article;
